7de9ff60f131462a0019d92a8d36cabdcf7ee2dd
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2022-04-19 16:44:06 +0100

    update

--- myPortfolio_project/projects/views.py
+++ myPortfolio_project/projects/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from django.http import request
+
 from .forms import ProjectForm
+from projects.models import Project
 
 
 # Create your views here.
 def create_projects(request):
-    forms = ProjectForm()
+    form = ProjectForm()
     print(request, "====")
-    print(forms)
+    print(form)
     data = request.POST
     print(data, "===data===")
     if request.method == "POST":
         form = ProjectForm(data=data)
-        if form.is_valid():
-            form.save()
-        else:
-            print(form.errors)
-    return render(request, 'projects/create_project.html', {"form": forms})
+        # form.save
+        # if form.is_valid():
+        #     form.save()
+        #     form = ProjectForm()
+        #
+        # else:
+        #     print(form.errors)
+    return render(request, 'projects/create_project.html', {"form": form})
+
+
+def get_projects(request):
+    projects = Project.objects.all()
+    return render(request, "projects/project_list.html", {"projects": projects})
